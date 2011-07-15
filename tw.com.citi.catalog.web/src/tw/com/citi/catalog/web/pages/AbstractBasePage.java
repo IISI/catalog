5f13f547dfafb0e5c53d1909e61defe8b4871e30
Chih-Liang Chang <itscchang@gmail.com>
2011-07-15 16:06:35 +0800

    在最上層的 AbstractBasePage 當中加上一個 try catch 來處理所有未被處理的 exception。

--- tw.com.citi.catalog.web/src/tw/com/citi/catalog/web/pages/AbstractBasePage.java
+++ tw.com.citi.catalog.web/src/tw/com/citi/catalog/web/pages/AbstractBasePage.java
@@ -1,18 +1,32 @@
 package tw.com.citi.catalog.web.pages;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.RequestCycle;
+import org.apache.wicket.protocol.http.WebResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import tw.com.citi.catalog.web.grid.IGridHandler;
 import tw.com.iisi.aquarius.web.AbstractAquariusPage;
 
 public abstract class AbstractBasePage extends AbstractAquariusPage {
 
+    final Logger logger = LoggerFactory.getLogger(AbstractBasePage.class);
+
     @Override
     public final String handleAjaxRequest(PageParameters params) {
-        if (params.containsKey("_gridHandler")) {
-            return getGridHandler(params.getString("_gridHandler")).handle(params);
-        } else {
-            return handleRequest(params);
+        try {
+            if (params.containsKey("_gridHandler")) {
+                return getGridHandler(params.getString("_gridHandler")).handle(params);
+            } else {
+                return handleRequest(params);
+            }
+        } catch (Exception e) {
+            logger.error("Internal server error.", e);
+            ((WebResponse) RequestCycle.get().getResponse()).getHttpServletResponse().setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return "{\"type\":\"" + e.getClass().getName() + "\",\"message\":\"" + e.getMessage() + "\"}";
         }
     }
 
