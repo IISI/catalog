5f8bda1b5a2ca9c1ba75c6605dfb2970a7144c07
Chih-Liang Chang <itscchang@gmail.com>
2011-07-25 14:17:17 +0800

    增加 db transaction 的設定，讓每一個 ajax request 都是一個獨立的 transaction。

--- tw.com.citi.catalog.web/src/tw/com/citi/catalog/web/pages/AbstractBasePage.java
+++ tw.com.citi.catalog.web/src/tw/com/citi/catalog/web/pages/AbstractBasePage.java
@@ -5,8 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.RequestCycle;
 import org.apache.wicket.protocol.http.WebResponse;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import tw.com.citi.catalog.web.grid.IGridHandler;
 import tw.com.iisi.aquarius.web.AbstractAquariusPage;
@@ -15,19 +20,37 @@ public abstract class AbstractBasePage extends AbstractAquariusPage {
 
     final Logger logger = LoggerFactory.getLogger(AbstractBasePage.class);
 
+    @SpringBean(name = "txManager")
+    private PlatformTransactionManager txManager;
+
     @Override
-    public final String handleAjaxRequest(PageParameters params) {
-        try {
-            if (params.containsKey("_gridHandler")) {
+    public final String handleAjaxRequest(final PageParameters params) {
+        if (params.containsKey("_gridHandler")) {
+            try {
                 return getGridHandler(params.getString("_gridHandler")).handle(params);
-            } else {
-                return handleRequest(params);
+            } catch (Exception e) {
+                return handleException(e);
             }
-        } catch (Exception e) {
-            logger.error("Internal server error.", e);
-            ((WebResponse) RequestCycle.get().getResponse()).getHttpServletResponse().setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            return "{\"type\":\"" + e.getClass().getName() + "\",\"message\":\"" + e.getMessage() + "\"}";
+        } else {
+            TransactionTemplate transactionTemplate = new TransactionTemplate(txManager);
+            return transactionTemplate.execute(new TransactionCallback<String>() {
+                
+                @Override
+                public String doInTransaction(TransactionStatus status) {
+                    try {
+                        return handleRequest(params);
+                    } catch (Exception e) {
+                        return handleException(e);
+                    }
+                }
+            });
         }
+    }
+
+    private String handleException(Exception e) {
+        logger.error("Internal server error.", e);
+        ((WebResponse) RequestCycle.get().getResponse()).getHttpServletResponse().setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        return "{\"type\":\"" + e.getClass().getName() + "\",\"message\":\"" + e.getMessage() + "\"}";
     }
 
     public IGridHandler getGridHandler(String handlerName) {
