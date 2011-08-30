f50078c3722071f057685e714e7c2a922e963b19
thomaschan <thomaschan@thomaschan-PC>
2011-08-30 11:30:31 +0800

    remove System.out

--- tw.com.citi.commcypt/src/tw/com/citi/commcypt/util/BridgeEncode.java
+++ tw.com.citi.commcypt/src/tw/com/citi/commcypt/util/BridgeEncode.java
@@ -7,7 +7,7 @@ import org.apache.commons.codec.binary.Base64;
 public class BridgeEncode {
 
 	public static String encode(String srcStr) throws UnsupportedEncodingException{
-		System.out.println("encode Str=" + srcStr);
+		//System.out.println("encode Str=" + srcStr);
 		if(srcStr!=null){
 			return HexUtil.bytesToHex(Base64.encodeBase64(srcStr.getBytes("utf-8")));
 		}else{
