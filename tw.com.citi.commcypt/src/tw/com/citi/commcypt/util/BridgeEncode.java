ac01ebe5535a5747aa37aa0863d1f99883719009
thomaschan <thomaschan@thomaschan-PC>
2011-08-30 11:15:25 +0800

    修改 pass=null exception

--- tw.com.citi.commcypt/src/tw/com/citi/commcypt/util/BridgeEncode.java
+++ tw.com.citi.commcypt/src/tw/com/citi/commcypt/util/BridgeEncode.java
@@ -3,12 +3,17 @@ package tw.com.citi.commcypt.util;
 import java.io.UnsupportedEncodingException;
 
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.Hex;
 
 public class BridgeEncode {
 
 	public static String encode(String srcStr) throws UnsupportedEncodingException{
-		return HexUtil.bytesToHex(Base64.encodeBase64(srcStr.getBytes("utf-8")));
+		System.out.println("encode Str=" + srcStr);
+		if(srcStr!=null){
+			return HexUtil.bytesToHex(Base64.encodeBase64(srcStr.getBytes("utf-8")));
+		}else{
+			return "";
+		}
+		
 	}
 	
 }
