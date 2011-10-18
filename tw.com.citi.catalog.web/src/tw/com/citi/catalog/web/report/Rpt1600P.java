58402eaf7a08aad72e32b84f06bb5b5b382422c1
Chih-Liang Chang <itscchang@gmail.com>
2011-10-18 15:42:42 +0800

    JCS1600 的 pvcs status 報表少了一個檔案。

---
+++ tw.com.citi.catalog.web/src/tw/com/citi/catalog/web/report/Rpt1600P.java
@@ -0,0 +1,63 @@
+package tw.com.citi.catalog.web.report;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.data.JRMapCollectionDataSource;
+
+import org.apache.wicket.RequestCycle;
+import org.apache.wicket.protocol.http.WebRequest;
+
+import tw.com.citi.catalog.dao.IAppFileDao;
+import tw.com.citi.catalog.dao.IFunctionLogDao;
+import tw.com.citi.catalog.model.FunctionLog;
+import tw.com.citi.catalog.web.util.F;
+
+public class Rpt1600P implements IReport {
+
+    private IFunctionLogDao functionLogDao;
+
+    private IAppFileDao appFileDao;
+
+    @Override
+    public InputStream getReportFile() {
+        return getClass().getResourceAsStream("/WEB-INF/report/Rpt1600P.jasper");
+    }
+
+    @Override
+    public Map<String, Object> getReportParameters() {
+        WebRequest req = ((WebRequest) RequestCycle.get().getRequest());
+        long functionLogId = Long.valueOf(req.getParameter("functionLogId"));
+        FunctionLog functionLog = functionLogDao.findById(functionLogId);
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("userId", F.getCurrentUser());
+        params.put("scrNo", req.getParameter("scrNo"));
+        params.put("processDate", functionLog.getStartTime());
+        params.put("finishDate", functionLog.getEndTime());
+        params.put("appId", req.getParameter("appId"));
+        return params;
+    }
+
+    @Override
+    public Object getReportData() {
+        WebRequest req = ((WebRequest) RequestCycle.get().getRequest());
+        long functionLogId = Long.valueOf(req.getParameter("functionLogId"));
+        return new JRMapCollectionDataSource(appFileDao.find1600PReportData(functionLogId));
+    }
+
+    @Override
+    public Map<String, Object> getExporterParameters() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void setFunctionLogDao(IFunctionLogDao functionLogDao) {
+        this.functionLogDao = functionLogDao;
+    }
+
+    public void setAppFileDao(IAppFileDao appFileDao) {
+        this.appFileDao = appFileDao;
+    }
+
+}
